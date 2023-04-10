a67dcb93d95f28079f15123a1e82107812c17bf4
Елков Егор Михайлович <elkov.egor@gmail.com>
2023-04-10 17:20:08 +0300

    Authorization LETTTSSGOOOO

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    },
+    {
+        timestamps: true,
+    });
+
+export default mongoose.model('User', UserSchema);
